343267f7d44d115dea928e91dc11ca951ba1be0d
NataliaVerchenko <ver4.nata@gmail.com>
2018-01-16 23:36:10 +0000

    add script in section  about from carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('#logoCarousel').find('.item').each(function () {
+        var next = $(this).next();
+
+        for (var i = 0; i < 4; i++) {
+            if (next.length > 0) {
+                next.children(':first-child').clone().appendTo($(this));
+                next = next.next();
+            } else {
+                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+                next = $(this).siblings(':first').next();
+            }
+        }
+    });
 });
